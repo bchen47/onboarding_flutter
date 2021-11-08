e6ea059172a38e96268f2f5a8ea844d23c5e4c10
Bo Chen <bc@MBP-de-Bo.home>
2021-11-08 10:36:41 +0100

    Añadido input username y password bloc

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,11 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:prueba/pages/login/login.dart';
 import 'package:prueba/pages/login/welcome.dart';
 import 'package:prueba/pages/login/register_options.dart';
+import 'package:prueba/src/bloc/authentication_bloc.dart';
 import 'package:prueba/src/bloc/authentication_repository.dart';
 import 'package:prueba/src/bloc/user_repository.dart';
 import 'pages/splash/splash.dart';
@@ -28,20 +30,28 @@ class MyApp extends StatelessWidget {
   }) : super(key: key);
   final AuthenticationRepository authenticationRepository;
   final UserRepository userRepository;
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Image Sequence Animator Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.blue, scaffoldBackgroundColor: Colors.black),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/welcome': (context) => const WelcomePage(),
-        '/registerOptions': (context) => const RegisterOptions(),
-        '/login': (context) => const LoginPage()
-      },
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+          create: (_) => AuthenticationBloc(
+                authenticationRepository: authenticationRepository,
+                userRepository: userRepository,
+              ),
+          child: MaterialApp(
+            title: 'Image Sequence Animator Demo',
+            theme: ThemeData(
+                primarySwatch: Colors.blue,
+                scaffoldBackgroundColor: Colors.black),
+            initialRoute: '/',
+            routes: {
+              '/': (context) => const SplashPage(),
+              '/welcome': (context) => const WelcomePage(),
+              '/registerOptions': (context) => const RegisterOptions(),
+              '/login': (context) => const LoginPage()
+            },
+          )),
     );
   }
 }
--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:prueba/pages/utils/style.dart';
-import 'package:prueba/src/bloc/bloc.dart';
+import 'package:prueba/src/bloc/authentication_repository.dart';
+import 'package:prueba/pages/login/bloc/login_bloc.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({
@@ -13,22 +14,47 @@ class LoginPage extends StatelessWidget {
         appBar: Customs.appBar("Iniciar sesión"),
         extendBodyBehindAppBar: true,
         body: Container(
-          margin: const EdgeInsets.only(top: 60, left: 10, right: 10),
-          child: Column(children: [
-            emailField(),
-            passwordField(),
-            Container(margin: const EdgeInsets.only(top: 20)),
-            Container(
-                child: Customs.button(
-                    "INICIAR SESIÓN",
-                    () => {Navigator.pushNamed(context, "/login")},
-                    MaterialStateProperty.all(Colors.orange)))
-          ]),
-        ));
+            margin: const EdgeInsets.only(top: 60, left: 10, right: 10),
+            child: BlocProvider(
+              create: (context) {
+                return LoginBloc(
+                  authenticationRepository:
+                      RepositoryProvider.of<AuthenticationRepository>(context),
+                );
+              },
+              child: Column(children: [
+                emailField(),
+                passwordField(),
+                Container(margin: const EdgeInsets.only(top: 20)),
+                Container(
+                    child: Customs.button(
+                        "INICIAR SESIÓN",
+                        () => {Navigator.pushNamed(context, "/login")},
+                        MaterialStateProperty.all(Colors.orange)))
+              ]),
+            )));
   }
 
   Widget emailField() {
-    return StreamBuilder(
+    return BlocBuilder<LoginBloc, LoginState>(
+        buildWhen: (previous, current) => previous.username != current.username,
+        builder: (context, state) {
+          return TextField(
+            key: const Key('loginForm_usernameInput_textField'),
+            onChanged: (username) =>
+                context.read<LoginBloc>().add(LoginUsernameChanged(username)),
+            style: const TextStyle(color: Colors.grey),
+            decoration: InputDecoration(
+                errorText: state.username.invalid ? 'invalid username' : null,
+                hintText: "Email",
+                prefixIcon: const Icon(Icons.email, color: Colors.grey),
+                hintStyle: const TextStyle(color: Colors.grey),
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                )),
+          );
+        });
+    /*return StreamBuilder(
         stream: bloc.email,
         builder: (context, snapshot) {
           return TextField(
@@ -45,14 +71,35 @@ class LoginPage extends StatelessWidget {
                   borderSide: BorderSide(color: Colors.grey),
                 ),
               ));
-        });
+        });*/
   }
 
   Widget passwordField() {
     return BlocBuilder<LoginBloc, LoginState>(
         buildWhen: (previous, current) => previous.username != current.username,
         builder: (context, snapshot) {
-          return TextField(
+          return BlocBuilder<LoginBloc, LoginState>(
+            buildWhen: (previous, current) =>
+                previous.username != current.username,
+            builder: (context, state) {
+              return TextField(
+                  key: const Key('loginForm_passwordInput_textField'),
+                  onChanged: (username) => context
+                      .read<LoginBloc>()
+                      .add(LoginUsernameChanged(username)),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    errorText:
+                        state.password.invalid ? 'invalid password' : null,
+                    hintText: "Contraseña",
+                    hintStyle: const TextStyle(color: Colors.grey),
+                    prefixIcon: const Icon(Icons.lock, color: Colors.grey),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.cyan),
+                    ),
+                  ));
+            },
+            /*return TextField(
               obscureText: true,
               //onChanged: bloc.changePassword,
               style: const TextStyle(color: Colors.grey),
@@ -66,7 +113,7 @@ class LoginPage extends StatelessWidget {
                   prefixIcon: const Icon(Icons.lock, color: Colors.grey),
                   enabledBorder: const UnderlineInputBorder(
                     borderSide: BorderSide(color: Colors.cyan),
-                  ) /*,
+                  ) */ /*,
         suffixIcon: IconButton(
             icon: Icon(
               // Based on passwordVisible state choose the icon
@@ -77,7 +124,7 @@ class LoginPage extends StatelessWidget {
                ),  
                onPressed: const ()=>{},           
             ),*/
-                  ));
+          );
         });
   }
 }
