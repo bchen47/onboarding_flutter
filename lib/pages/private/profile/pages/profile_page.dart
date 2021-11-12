a69b822563da6c24306d7322aeeb1c5f009008f2
Bo Chen <bc@MacBook-Pro-de-Bo.local>
2021-11-12 15:45:05 +0100

    Trabajando en la página de perfil WIP

--- lib/pages/private/profile/pages/profile_page.dart
+++ lib/pages/private/profile/pages/profile_page.dart
@@ -16,22 +16,84 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(body: home());
   }
 
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white);
+  static const TextStyle optionStyleImageCardText =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white);
+
   Widget home() {
     return BlocBuilder<UserBloc, UserState>(builder: (context, state) {
       return Container(
-          color: Colors.grey,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(children: const [Text("Perfil")]),
+          color: Colors.grey[850],
+          child: Container(
+              margin: const EdgeInsets.only(top: 25.0),
+              child: Column(children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Perfil",
+                        style: optionStyle,
+                      )
+                    ]),
+                Container(
+                  margin: const EdgeInsets.only(top: 20.0),
+                ),
                 Image.network("https://bestcycling.com/" +
                     state.user.attributes["avatar_url"]),
-                const Text("Bestcycling S.L."),
-                Row(children: const [
-                  Icon(Icons.location_pin),
-                  Text("Alboraya, España")
+                Container(
+                    margin: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                    child: Text(
+                      "Bestcycling S.L.",
+                      style: TextStyle(fontSize: 20, color: Colors.grey[500]),
+                    )),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Icon(Icons.location_pin, color: Colors.grey[500]),
+                  Text(
+                    "Alboraya, España",
+                    style: TextStyle(color: Colors.grey[500]),
+                  )
                 ]),
-              ]));
+                Container(
+                    margin: const EdgeInsets.only(top: 20, bottom: 20),
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.only(right: 40.0, left: 40.0)),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    side: const BorderSide(
+                                        color: Colors.orange, width: 1),
+                                    borderRadius: BorderRadius.circular(30.0))),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.transparent)),
+                        onPressed: () => {},
+                        child: const Text("Editar"))),
+                middleStatsItemRow(state)
+              ])));
     });
+  }
+
+  Widget middleStatsItemRow(state) {
+    return Container(
+        padding: const EdgeInsets.all(30.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            MiddleStatsItem(
+                state.user.attributes["affiliation_data"]["level"].toString(),
+                "nivel"),
+            MiddleStatsItem(
+                state.user.attributes["affiliation_data"]["level"].toString(),
+                "nivel"),
+            MiddleStatsItem(
+                state.user.attributes["affiliation_data"]["level"].toString(),
+                "nivel")
+          ],
+        ));
+  }
+
+  Widget MiddleStatsItem(String level, String label) {
+    return Column(children: [Text(level), Text(label)]);
   }
 }
